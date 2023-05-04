d1001fd7f9e1391dfe2f165469e3e1a6cc46be8c
Aofekiko <aofekiko@gmail.com>
2023-05-04 22:26:50 +0000

    Debug for type error

--- track.py
+++ track.py
@@ -248,9 +248,10 @@ def run(
                             bbox_w = output[2] - output[0]
                             bbox_h = output[3] - output[1]
                             # Write MOT compliant results to file
+                            print(frame_idx + 1, id, bbox_left, bbox_top, bbox_w, bbox_h, cls, conf, trajectory, i)
                             with open(txt_path + '.txt', 'a') as f:
                                 f.write(('%g ' * 10 + '\n') % (frame_idx + 1, id, bbox_left,  # MOT format
-                                                               bbox_top, bbox_w, bbox_h, cls, conf, trajectory, i))
+                                                               bbox_top, bbox_w, bbox_h, cls, conf, -1, i))
 
                         if save_vid or save_crop or show_vid:  # Add bbox/seg to image
                             c = int(cls)  # integer class
